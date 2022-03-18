702408fc932334f9530893182f0c37c6ef1e78ac
Brian Teets <bwt17@msn.com>
2022-03-18 17:35:44 -0600

    added comments to java file

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -3,7 +3,8 @@ package com.skilldistillery.makechange;
 public class CashRegister {
 
 	public static void main(String[] args) {
-		
+		// For this hw, it is okay to have everything inside main
+			// creating other methods is optional 
 		
 	}
 
