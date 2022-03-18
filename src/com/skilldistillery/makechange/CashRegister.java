60c48fb2af21906c3d68792ce18dbb3deb968102
Brian Teets <bwt17@msn.com>
2022-03-18 17:53:20 -0600

    import scanner, added a userInput variable

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -1,14 +1,22 @@
 package com.skilldistillery.makechange;
 
+import java.util.Scanner; 
+
 public class CashRegister {
 
 	public static void main(String[] args) {
 		// For this hw, it is okay to have everything inside main
 			// creating other methods is optional 
 		
-		double amount;
-		double price; 
+		Scanner sc = new Scanner(System.in); 
 		
+		double amount = 0;
+		double price = 0; 
+		// TODO add sc variable
+		double userInput = sc.nextDouble(); 
+		
+		// TODO don't forget to close the scanner!!! 
+		sc.close(); 
 	}
 
 }
