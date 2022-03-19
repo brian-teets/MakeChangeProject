026b0684958c8b89b2665077677861a0d329315c
Brian Teets <bwt17@msn.com>
2022-03-19 10:03:03 -0600

    not sure if i'm on the right track, but initialized variables for each type of denomination up to 20

--- src/com/skilldistillery/makechange/CashRegister.java
+++ src/com/skilldistillery/makechange/CashRegister.java
@@ -6,7 +6,7 @@ public class CashRegister {
 
 	public static void main(String[] args) {
 		// For this hw, it is okay to have everything inside main
-		// creating other methods is optional
+			// creating other methods is optional
 
 		Scanner sc = new Scanner(System.in);
 
@@ -19,7 +19,7 @@ public class CashRegister {
 		// reassign purchasePrice to the value of the userInput
 		purchasePrice = userInputPrice;
 		// TODO User Story #2: prompt user to ask how much money was tendered by the
-		// customer
+			// customer
 		System.out.println("How much money was tendered by the customer? ");
 		// TODO add scanner variable to store user input for amount tendered by customer 
 		double userInputAmt = sc.nextDouble();
@@ -28,13 +28,27 @@ public class CashRegister {
 			// too little ( < ) money, OR the exact amount.
 		if (amountTendered == purchasePrice) {
 			System.out.println("The customer supplied the exact amount. This transaction is complete! ");
-		} else if (amountTendered < purchasePrice) {
+		} 
+		else if (amountTendered < purchasePrice) {
 			System.out.println("The customer didn't supply enough money for the item purchased. ");
 		}
 		// TODO User Story #4: If the amount tendered is more than the cost of the item,
 			// display the number of bills and coins that should be given to the customer. 
-		else {
-			
+		else if(amountTendered > purchasePrice){
+			System.out.println("The customer overpaid. You need to make change. ");
+			System.out.println(" ... calculating change needed, one moment ... "); 
+			// TODO Seem to need some way of storing current count or tally and continuing to make change 
+				// test conditions in assignment will only check up through amount tendered of 20.00 
+			double changeRequired = 0.0;
+			double tenDollarBill = 0.0;
+			double fiveDollarBill = 0.0;
+			double quarter = 0.0;
+			double dime = 0.0;
+			double nickel = 0.0; 
+			double penny = 0.0; 
+			if(amountTendered % 20.0 == 0) {  // Check if amountTendered divisible by 20?
+				
+			} 
 		}
 
 		// TODO don't forget to close the scanner!
